2cf455a45b794f05a2d082b4f7fa3cabcc63a8b8
ARSENIO <ARSENIO@ARSENIO-LANGA-SSI.ge.mz>
2023-09-11 14:30:32 +0100

    Project init

---
+++ APIRestful - TodoList/src/main/java/com/place/apirestful/ApiRestPicPay1Application.java
@@ -0,0 +1,13 @@
+package com.place.apirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestPicPay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestPicPay1Application.class, args);
+	}
+
+}
---
+++ APIRestful - TodoList/src/test/java/com/place/apirestful/ApiRestPicPay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.place.apirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestPicPay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
