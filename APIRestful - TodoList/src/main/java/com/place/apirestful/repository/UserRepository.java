ffc33d5cacf29e476d409af4c55aa2a4da96a92f
ARSENIO <ARSENIO@ARSENIO-LANGA-SSI.ge.mz>
2023-09-15 08:13:08 +0100

    Create UserRepository

---
+++ APIRestful - TodoList/src/main/java/com/place/apirestful/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.place.apirestful.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.place.apirestful.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
