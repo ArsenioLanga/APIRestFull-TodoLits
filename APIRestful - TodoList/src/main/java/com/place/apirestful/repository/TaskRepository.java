246088a1b998f65b3ba25f61c6ebc9a67f4bfe0f
ARSENIO <ARSENIO@ARSENIO-LANGA-SSI.ge.mz>
2023-09-15 10:10:12 +0100

    Other form for build queries

--- APIRestful - TodoList/src/main/java/com/place/apirestful/repository/TaskRepository.java
+++ APIRestful - TodoList/src/main/java/com/place/apirestful/repository/TaskRepository.java
@@ -1,9 +1,18 @@
 package com.place.apirestful.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.place.apirestful.models.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
 
+	List<Task> findByUser_id(Long id);
+	
+//	@Query(value = "SELECT task FROM Task task WHERE task.user.id = :")
+//	List<Task> findByUser_id(@Param("id") Long id);
+//	
+//	@Query(value="select * from tb_tasks  where user_id = :id", nativeQuery = true)
+//	List<Task> qualquerNome(@Param("id") Long id);
 }
